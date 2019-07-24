8da77e03362e6ad2507f2ac7726602919d4f12f4
liuqi <754493376@qq.com>
2019-07-24 19:16:57 +0800

    费用计算demo

---
+++ JSDemo/libs/cityList.js
@@ -0,0 +1,35 @@
+
+
+var cityListJson=[
+    //0.55每公斤
+    {id:0,key:0,name:'潜江',kmSubsidy:0,pSubsidy:0},
+    {id:1,key:0,name:'仙桃',kmSubsidy:58,pSubsidy:232.00},
+    {id:2,key:0,name:'洪湖',kmSubsidy:118.00,pSubsidy:472.00},
+    {id:3,key:0,name:'天门',kmSubsidy:59.00,pSubsidy:236.00},
+    {id:4,key:0,name:'监利',kmSubsidy:99.00,pSubsidy:396.00},
+    //0.60
+    {id:5,key:1,name:'荆州',kmSubsidy:0,pSubsidy:0},
+    {id:6,key:1,name:'公安',kmSubsidy:37.00,pSubsidy:148.00},
+    {id:7,key:1,name:'江陵',kmSubsidy:47.00,pSubsidy:188.00},
+    {id:8,key:1,name:'松滋',kmSubsidy:57.00,pSubsidy:228.00},
+    {id:9,key:1,name:'枝江',kmSubsidy:54.00,pSubsidy:216.00},
+    {id:10,key:1,name:'荆门',kmSubsidy:0.00,pSubsidy:0.00},
+    {id:11,key:1,name:'沙洋',kmSubsidy:66.12,pSubsidy:264.48},
+    {id:12,key:1,name:'钟祥',kmSubsidy:65.00,pSubsidy:260.00},
+    //0.65
+    {id:13,key:2,name:'宜昌',kmSubsidy:0.00,pSubsidy:0.00},
+    {id:14,key:2,name:'秭归',kmSubsidy:59.00,pSubsidy:236.00},
+    {id:15,key:2,name:'当阳',kmSubsidy:73.00,pSubsidy:292.00},
+    {id:16,key:2,name:'猇亭',kmSubsidy:32.00,pSubsidy:128.00},
+    {id:17,key:2,name:'宜都',kmSubsidy:51.00,pSubsidy:204.00},
+    {id:18,key:2,name:'襄阳',kmSubsidy:0.00,pSubsidy:0.00},
+    {id:19,key:2,name:'枣阳',kmSubsidy:86.00,pSubsidy:344.00},
+    {id:20,key:2,name:'宜城',kmSubsidy:56.00,pSubsidy:224.00},
+    {id:21,key:2,name:'南漳',kmSubsidy:42.00,pSubsidy:260.00},
+    {id:22,key:2,name:'谷城',kmSubsidy:63.00,pSubsidy:168.00},
+
+]
+export var cityData={
+    cityListJson:cityListJson
+}
+
