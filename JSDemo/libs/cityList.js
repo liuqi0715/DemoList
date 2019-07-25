b43d2d4c467e1045b25ab69d71a91c7231447461
liuqi <754493376@qq.com>
2019-07-25 09:09:33 +0800

    计算功能优化

--- JSDemo/libs/cityList.js
+++ JSDemo/libs/cityList.js
@@ -1,13 +1,14 @@
 
 
 var cityListJson=[
-    //0.55每公斤
+    //淡蓝色
+    
     {id:0,key:0,name:'潜江',kmSubsidy:0,pSubsidy:0},
     {id:1,key:0,name:'仙桃',kmSubsidy:58,pSubsidy:232.00},
     {id:2,key:0,name:'洪湖',kmSubsidy:118.00,pSubsidy:472.00},
     {id:3,key:0,name:'天门',kmSubsidy:59.00,pSubsidy:236.00},
     {id:4,key:0,name:'监利',kmSubsidy:99.00,pSubsidy:396.00},
-    //0.60
+    //紫色
     {id:5,key:1,name:'荆州',kmSubsidy:0,pSubsidy:0},
     {id:6,key:1,name:'公安',kmSubsidy:37.00,pSubsidy:148.00},
     {id:7,key:1,name:'江陵',kmSubsidy:47.00,pSubsidy:188.00},
@@ -16,7 +17,7 @@ var cityListJson=[
     {id:10,key:1,name:'荆门',kmSubsidy:0.00,pSubsidy:0.00},
     {id:11,key:1,name:'沙洋',kmSubsidy:66.12,pSubsidy:264.48},
     {id:12,key:1,name:'钟祥',kmSubsidy:65.00,pSubsidy:260.00},
-    //0.65
+    //蓝色底纹
     {id:13,key:2,name:'宜昌',kmSubsidy:0.00,pSubsidy:0.00},
     {id:14,key:2,name:'秭归',kmSubsidy:59.00,pSubsidy:236.00},
     {id:15,key:2,name:'当阳',kmSubsidy:73.00,pSubsidy:292.00},
@@ -27,6 +28,54 @@ var cityListJson=[
     {id:20,key:2,name:'宜城',kmSubsidy:56.00,pSubsidy:224.00},
     {id:21,key:2,name:'南漳',kmSubsidy:42.00,pSubsidy:260.00},
     {id:22,key:2,name:'谷城',kmSubsidy:63.00,pSubsidy:168.00},
+
+    //红色
+    {id:23,key:3,name:'随州',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:24,key:3,name:'随县',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:25,key:3,name:'广水',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:26,key:3,name:'黄石',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:27,key:3,name:'大冶',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:28,key:3,name:'阳新',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:29,key:3,name:'黄石港',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:30,key:3,name:'浠水',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:31,key:3,name:'黄梅',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:32,key:3,name:'鄂州',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:33,key:3,name:'黄冈',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:34,key:3,name:'黄冈',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:35,key:3,name:'黄冈',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:36,key:3,name:'蕲春',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:37,key:3,name:'华容',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:38,key:3,name:'咸宁',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:39,key:3,name:'赤壁',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:40,key:3,name:'嘉鱼',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:41,key:3,name:'通山',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:42,key:3,name:'通城',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:43,key:3,name:'崇阳',kmSubsidy:63.00,pSubsidy:168.00},
+    //绿色
+    {id:44,key:4,name:'十堰',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:45,key:4,name:'老河口',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:46,key:4,name:'丹江口',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:47,key:4,name:'郧阳',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:48,key:4,name:'武当山',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:49,key:4,name:'房县',kmSubsidy:63.00,pSubsidy:168.00},
+    //黑色
+    {id:50,key:5,name:'黄陂',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:51,key:5,name:'新洲',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:52,key:5,name:'红安',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:53,key:5,name:'麻城',kmSubsidy:63.00,pSubsidy:168.00},
+    //黄色
+    {id:54,key:6,name:'孝感',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:55,key:6,name:'应城',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:56,key:6,name:'安陆',kmSubsidy:63.00,pSubsidy:168.00},
+    {id:57,key:6,name:'汉川',kmSubsidy:63.00,pSubsidy:168.00},
+
+
+
+
+
+
+
+
 
 ]
 export var cityData={
